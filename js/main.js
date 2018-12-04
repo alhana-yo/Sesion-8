11ca45405cd0943a7f687e617a555a82345f0ef4
alhana-yo <beatriz.german.rengel@gmail.com>
2018-12-04 09:47:45 +0100

    Final exercise finished

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 let face = document.querySelector('.face');
+let head = document.querySelector('.head');
 
 function laugh(e) {
     face.innerHTML =';)';
 }
 
+function initialPosition(e){
+    face.innerHTML =':)';
+    console.log(face);
+}
+
 face.addEventListener('click', laugh );
+head.addEventListener('mouseout', initialPosition);
+
+
